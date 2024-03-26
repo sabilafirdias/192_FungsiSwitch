62eba64c59d134258ee3bda89736e6b66906c25e
sabilafiridas <sabilafirdias@gmail.com>
2024-03-26 10:43:22 +0700

    memanggil dengan looping do

--- SwitchCase/SwitchCase.cpp
+++ SwitchCase/SwitchCase.cpp
@@ -31,7 +31,19 @@ void inputData() {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int pilihan;
+    do {
+        system("CLS");
+        cout << "Menu kalkulator sederhana" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukkan pilihan: ";
+        cin >> pilihan;
+
+    }
 }
 
 
