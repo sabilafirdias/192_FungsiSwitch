b9d071d87411492418acce0120e3faaf852c8460
sabilafiridas <sabilafirdias@gmail.com>
2024-03-26 10:29:16 +0700

    penjumlahan dan pengurangan

--- SwitchCase/SwitchCase.cpp
+++ SwitchCase/SwitchCase.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
+int penjumlahan(int a, int b) {
+    return a + b;
+}
+
+int pengurangan(int a, int b) {
+    return a - b;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
