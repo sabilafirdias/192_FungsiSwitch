9d862216ee9146a1d515132b1f283520b4865884
sabilafiridas <sabilafirdias@gmail.com>
2024-03-26 10:36:12 +0700

    membuat prosedur input data

--- SwitchCase/SwitchCase.cpp
+++ SwitchCase/SwitchCase.cpp
@@ -22,6 +22,13 @@ float pembagian(int a, int b) {
     return a / b;
 }
 
+void inputData() {
+    cout << "\nMasukkan bilangan pertama: ";
+    cin >> bilangan1;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> bilangan2;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
